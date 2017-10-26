9b6c2da9e8bb7c9b04916d14fa898ac8a90920b9
Sondre <sondre.kongsgaard@gmail.com>
2017-10-25 21:45:15 -0700

    Add plot 1 data

--- Omnibot/Lab2a_template_rtt/Lab2a_template_dt.h
+++ Omnibot/Lab2a_template_rtt/Lab2a_template_dt.h
@@ -5,7 +5,7 @@
  *
  * Model version              : 1.37
  * Simulink Coder version : 8.6 (R2014a) 27-Dec-2013
- * C source code generated on : Wed Oct 25 21:17:53 2017
+ * C source code generated on : Wed Oct 25 21:42:34 2017
  *
  * Target selection: realtime.tlc
  * Embedded hardware selection: ARM Compatible->ARM 7
@@ -75,7 +75,7 @@ static DataTypeTransitionTable rtBTransTable = {
 
 /* data type transitions for Parameters structure */
 static DataTypeTransition rtPTransitions[] = {
-  { (char_T *)(&Lab2a_template_P.Kd), 0, 0, 632 },
+  { (char_T *)(&Lab2a_template_P.Kd), 0, 0, 626 },
 
   { (char_T *)(&Lab2a_template_P.timeresetswitch_CurrentSetting), 3, 0, 1 }
 };
--- Omnibot/lab2.m
+++ Omnibot/lab2.m
@@ -27,6 +27,7 @@ traj_C = q(3, :);
 traj_dC = q_dot(3, :);
 
 %% Circular trajectory
+%{
 radius = 0.5;      % Circle radius (meters)
 center = [0, 0.5]; % Center of the circle (meters)
 resolution = 100;  % Number of points along the circle perimeter
@@ -40,3 +41,4 @@ traj_B = q(2, :);
 traj_dB = q_dot(2, :);
 traj_C = q(3, :);
 traj_dC = q_dot(3, :);
+%}
