92cd53888dacd46a584feecb9f5e17d65037bcfa
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-10-25 21:53:47 -0700

    Fix plotting

--- Omnibot/plots.m
+++ Omnibot/plots.m
@@ -2,62 +2,62 @@
 %% Plot 1
 % Square trajectory: Encoder data for the three motors along with the reference trajectories in a single subplot.
 
-% load('plot1.mat');
+load('mat-files/plot1.mat');
 
-figure(1); clf; hold on;
-subplot(3, 1, 1);
-%plot(simout_ref_A.time, simout_ref_A.signals.values)
-%plot(simout_A.time, simout_A.signals.values);
+figure(1); clf;
+subplot(3, 1, 1); hold on;
+plot(simout_ref_A.time, simout_ref_A.signals.values)
+plot(simout_A.time, simout_A.signals.values);
 title('Motor A trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 2);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_B.time, simout_B.signals.values);
+subplot(3, 1, 2); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_B.time, simout_B.signals.values);
 title('Motor B trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 3);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_C.time, simout_C.signals.values);
+subplot(3, 1, 3); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_C.time, simout_C.signals.values);
 title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
 %% Plot 2
 % Circle trajectory: Encoder data for the three motors along with the reference trajectories in a single subplot.
 
-% load('plot2.mat'); Encoder data for the three motors along with the reference trajectories in a single subplot.
+load('mat-files/plot2.mat');
 
-figure(2); clf; hold on;
-subplot(3, 1, 1);
-%plot(simout_ref_A.time, simout_ref_A.signals.values)
-%plot(simout_A.time, simout_A.signals.values);
+figure(2); clf;
+subplot(3, 1, 1); hold on;
+plot(simout_ref_A.time, simout_ref_A.signals.values)
+plot(simout_A.time, simout_A.signals.values);
 title('Motor A trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 2);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_B.time, simout_B.signals.values);
+subplot(3, 1, 2); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_B.time, simout_B.signals.values);
 title('Motor B trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 3);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_C.time, simout_C.signals.values);
+subplot(3, 1, 3); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_C.time, simout_C.signals.values);
 title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
 %% Plot 3
 % Our best performance trajectory (square/circle) after improvements:
@@ -69,29 +69,29 @@ legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interprete
 % - Change the speed and/or amount of smoothness used in trajectory
 % playback
 
-% load('plot3.mat');
+load('mat-files/plot3.mat');
 
-figure(3); clf; hold on;
-subplot(3, 1, 1);
-%plot(simout_ref_A.time, simout_ref_A.signals.values)
-%plot(simout_A.time, simout_A.signals.values);
+figure(3); clf;
+subplot(3, 1, 1); hold on;
+plot(simout_ref_A.time, simout_ref_A.signals.values)
+plot(simout_A.time, simout_A.signals.values);
 title('Motor A trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 2);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_B.time, simout_B.signals.values);
+subplot(3, 1, 2); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_B.time, simout_B.signals.values);
 title('Motor B trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
 
-subplot(3, 1, 3);
-%plot(simout_ref_B.time, simout_ref_B.signals.values)
-%plot(simout_C.time, simout_C.signals.values);
+subplot(3, 1, 3); hold on;
+plot(simout_ref_B.time, simout_ref_B.signals.values)
+plot(simout_C.time, simout_C.signals.values);
 title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'$Reference trajectory$', '$Encoder data$'}, 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex');
