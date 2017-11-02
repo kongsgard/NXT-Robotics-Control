f8e1a36c7fecf50b0dfddcdf5198e71c0833d1b2
Sondre <sondre.kongsgaard@gmail.com>
2017-11-01 22:02:22 -0700

    Use for-loop for cumsum

--- Omnibot/calculateJointTrajectoriesImproved.m
+++ Omnibot/calculateJointTrajectoriesImproved.m
@@ -7,11 +7,15 @@ v_x = [0, diff(x)/dt];
 v_y = [0, diff(y)/dt];
 
 q_dot = zeros(3,length(v_x));
+q = zeros(3,length(v_x));
 for i=1:length(v_x)
     q_dot(:,i) = get_F(phi(i)) * [v_x(i); v_y(i); phi(i)];
+    if i ~= 1
+        q(:,i) = q(:,i-1) + q_dot(:, i) * dt;
+    end
 end
 
-q = (cumsum(q_dot')* dt)';
+% q = (cumsum(q_dot')* dt)';
 
 t = linspace(0, length(q)*dt, length(q));
 
