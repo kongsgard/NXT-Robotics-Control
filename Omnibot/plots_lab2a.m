1e40ee0b32bb078df4881a0bad586d37cea30b66
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-11-11 22:31:43 -0800

    Add more plots

--- Omnibot/plots_lab2a.m
+++ Omnibot/plots_lab2a.m
@@ -2,7 +2,7 @@
 %% Plot 1
 % Square trajectory: Encoder data for the three motors along with the reference trajectories in a single subplot.
 
- % load('mat-files/plot1.mat');
+load('mat-files/plot1.mat');
 
 figure(1); clf;
 subplot(3, 1, 1); hold on;
@@ -11,7 +11,15 @@ plot(simout_A.time, simout_A.signals.values);
 title('Motor A trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'southwest');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+ti = find(simout_A.time >= 25.8, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_A.time >= 24.6, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
 
 subplot(3, 1, 2); hold on;
 plot(simout_ref_B.time, simout_ref_B.signals.values)
@@ -21,6 +29,14 @@ xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'southwest');
 
+ti = find(simout_B.time >= 49, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_B.time >= 30, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
+
 subplot(3, 1, 3); hold on;
 plot(simout_ref_C.time, simout_ref_C.signals.values)
 plot(simout_C.time, simout_C.signals.values);
@@ -28,6 +44,14 @@ title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+ti = find(simout_C.time >= 25.8, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_C.time >= 24.6, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
 
 %% Plot 2
 % Circle trajectory: Encoder data for the three motors along with the reference trajectories in a single subplot.
@@ -43,6 +67,14 @@ xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
 
+ti = find(simout_A.time >= 23.5, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_A.time >= 23, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
+
 subplot(3, 1, 2); hold on;
 plot(simout_ref_B.time, simout_ref_B.signals.values)
 plot(simout_B.time, simout_B.signals.values);
@@ -51,6 +83,14 @@ xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'southwest');
 
+ti = find(simout_B.time >= 28, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_B.time >= 23.7, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
+
 subplot(3, 1, 3); hold on;
 plot(simout_ref_C.time, simout_ref_C.signals.values)
 plot(simout_C.time, simout_C.signals.values);
@@ -58,6 +98,14 @@ title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+ti = find(simout_C.time >= 23.5, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_C.time >= 22.9, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
 
 %% Plot 3
 % Our best performance trajectory (square/circle) after improvements:
@@ -80,13 +128,29 @@ xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
 
+ti = find(simout_A.time >= 22.98, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_A.time >= 22.7, 1);
+text(simout_A.time(ti - 15), simout_A.signals.values(ti), 'O');
+text(simout_A.time(ti + 30), simout_A.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
+
 subplot(3, 1, 2); hold on;
 plot(simout_ref_B.time, simout_ref_B.signals.values)
 plot(simout_B.time, simout_B.signals.values);
 title('Motor B trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
-legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'southwest');
+
+ti = find(simout_B.time >= 28, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_B.time >= 23.7, 1);
+text(simout_B.time(ti - 15), simout_B.signals.values(ti), 'O');
+text(simout_B.time(ti + 30), simout_B.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
 
 subplot(3, 1, 3); hold on;
 plot(simout_ref_C.time, simout_ref_C.signals.values)
@@ -94,4 +158,57 @@ plot(simout_C.time, simout_C.signals.values);
 title('Motor C trajectory', 'FontSize', 30, 'Interpreter', 'Latex');
 xlabel('Time (seconds)', 'FontSize', 20, 'Interpreter', 'Latex');
 ylabel('Angle (rad)', 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+ti = find(simout_C.time >= 23, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$v_{max}$', 'Interpreter', 'Latex');
+
+ti = find(simout_C.time >= 22.6, 1);
+text(simout_C.time(ti - 15), simout_C.signals.values(ti), 'O');
+text(simout_C.time(ti + 30), simout_C.signals.values(ti), '$a_{max}$', 'Interpreter', 'Latex');
+
+%% Plot 4
+
+load('mat-files/plot1.mat');
+
+A = simout_A.signals.values;
+B = simout_B.signals.values;
+C = simout_C.signals.values;
+xy_a = getOmnibotJacobian(0) * [A'; B'; C'];
+plot(xy_a(1,:), xy_a(2,:)); hold on;
+plot(x, y);
+title('Square trajectory of the center of body on the x-y plane', 'FontSize', 30, 'Interpreter', 'Latex');
+xlabel('x (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
+ylabel('y (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+%% Plot 5
+
+load('mat-files/plot2.mat');
+
+A = simout_A.signals.values;
+B = simout_B.signals.values;
+C = simout_C.signals.values;
+xy_a = getOmnibotJacobian(0) * [A'; B'; C'];
+plot(xy_a(1,:), xy_a(2,:)); hold on;
+plot(x, y);
+title('Circular trajectory of the center of body on the x-y plane', 'FontSize', 30, 'Interpreter', 'Latex');
+xlabel('x (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
+ylabel('y (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
+legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
+
+%% Plot 6
+
+load('mat-files/plot3.mat');
+
+A = simout_A.signals.values;
+B = simout_B.signals.values;
+C = simout_C.signals.values;
+xy_a = getOmnibotJacobian(0) * [A'; B'; C'];
+plot(xy_a(1,:), xy_a(2,:)); hold on;
+plot(x, y);
+title('Circular trajectory of the center of body on the x-y plane', 'FontSize', 30, 'Interpreter', 'Latex');
+xlabel('x (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
+ylabel('y (meters)', 'FontSize', 20, 'Interpreter', 'Latex');
 legend({'Reference trajectory', 'Encoder data'}, 'FontSize', 20, 'Interpreter', 'Latex', 'Location', 'northwest');
